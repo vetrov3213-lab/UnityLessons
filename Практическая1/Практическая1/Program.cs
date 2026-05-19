d62fad6b3b15bd6fcf455c52912892858d37166f
Katya <MyFaceIsMyMood@gmail.com>
2026-05-19 22:24:52 +0300

    сделала класс

---
+++ Практическая1/Практическая1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Практическая1
+{
+    internal class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    public class Inventory
+    {
+        public float MaxWeight { get; }
+        List<InventoryItem> _items = new List<InventoryItem>();
+        public Inventory(int maxWeight)
+        {
+            MaxWeight = maxWeight;
+            _items = new List<InventoryItem>();
+        }
+        public float GetCurrentWeight()
+        {
+            float itemsweight = 0f;
+            foreach (InventoryItem item in _items)
+            {
+
+                itemsweight = item.Weight + itemsweight;
+
+            }
+            return itemsweight;
+        }
+        public bool TryAdd(InventoryItem item)
+        {
+            if (item.Weight + GetCurrentWeight() < MaxWeight)
+            {
+                _items.Add(item);
+                return true;
+            }
+            return false;
+        }
+    }
+    public class InventoryItem
+    {
+        public float Weight { get; }
+        public InventoryItem(int weight)
+        {
+            Weight = weight;
+        }
+    }
+}
